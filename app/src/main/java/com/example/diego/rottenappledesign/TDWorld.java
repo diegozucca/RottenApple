78489a064da43130f0b7e286cbebb269a2b8a5e4
diego <diego.zucca@studenti.unipd.it>
2016-06-03 19:40:42 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	RottenApple.iml
    #	app/app.iml

--- app/src/main/java/com/example/diego/rottenappledesign/TDWorld.java
+++ app/src/main/java/com/example/diego/rottenappledesign/TDWorld.java
@@ -1,4 +1,11 @@
 package com.example.diego.rottenappledesign;
+/*
+*
+* Activity che mostra alcuni componenti descrivendone il parametro elevation
+*
+* @author RottenApple
+* @version 1.0
+* */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
